97e21966808c7915b4004d78a5cca9119585ab73
Xu Qian <1464238196@qq.com>
2024-08-07 19:38:15 +0800

    add baidu test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -5,6 +5,11 @@ import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import com.example.demo.model.LoginRequest;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +38,17 @@ public class TestController {
         }
         log.info("result:{}", result);
         return result;
+    }
+
+    @PostMapping("/baidu")
+    public void testBaidu() {
+       try(CloseableHttpClient httpClient = HttpClientBuilder.create().build()){
+            HttpGet get = new HttpGet("https://www.baidu.com");
+            CloseableHttpResponse response = httpClient.execute(get);
+            log.info("百度 response:{}",response);
+       }catch (Exception e){
+           log.error("error",e);
+       }
     }
 
 }
